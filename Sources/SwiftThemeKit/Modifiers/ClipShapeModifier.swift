c96425386cc3c1fb8a31845877d189bd1637c145
Eduard Albu <eduardalbu@gmail.com>
2025-05-14 21:27:34 +0300

    Added more configurations

---
+++ Sources/SwiftThemeKit/Modifiers/ClipShapeModifier.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+struct ClipShapeModifier: ViewModifier {
+  @Environment(\.appTheme) private var theme
+  let token: ShapeToken
+
+  func body(content: Content) -> some View {
+    content.clipShape(theme.shapes[token])
+  }
+}
--- Sources/SwiftThemeKit/Modifiers/CornerRadiusModifier.swift
+++ Sources/SwiftThemeKit/Modifiers/CornerRadiusModifier.swift
@@ -1,10 +1,10 @@
 import SwiftUI
 
 struct CornerRadiusModifier: ViewModifier {
-    @Environment(\.appTheme) private var theme
-    let token: RadiusToken
-
-    func body(content: Content) -> some View {
-        content.cornerRadius(theme.radii[token])
-    }
+  @Environment(\.appTheme) private var theme
+  let token: RadiusToken
+  
+  func body(content: Content) -> some View {
+    content.cornerRadius(theme.radii[token])
+  }
 }
