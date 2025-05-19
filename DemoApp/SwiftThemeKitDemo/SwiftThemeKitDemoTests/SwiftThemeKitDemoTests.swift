d1b2c0754ae8ebf29de09b65561ef8884b390e5a
Eduard Albu <eduard.albu@voya.ai>
2025-05-19 14:04:59 +0300

    Updated snapshots

--- DemoApp/SwiftThemeKitDemo/SwiftThemeKitDemoTests/SwiftThemeKitDemoTests.swift
+++ DemoApp/SwiftThemeKitDemo/SwiftThemeKitDemoTests/SwiftThemeKitDemoTests.swift
@@ -6,24 +6,24 @@ import SwiftUI
 @MainActor
 struct SwiftThemeKitDemoTests {
   @Test func testExampleViewSnapshot() {
-    let buttonsView = AnyView(ButtonsView(shapes: .rounded))
-    let checkboxesView = AnyView(CheckboxesView())
-    let colorsView = AnyView(ColorsView())
-    let radiiView = AnyView(RadiiView())
-    let radioGroupView = AnyView(RadioGroupView(shapes: .constant(.rounded)))
-    let shadowsView = AnyView(ShadowsView())
-    let shapesView = AnyView(ShapesView())
-    let spacingsView = AnyView(SpacingsView())
-    let strokesView = AnyView(StrokesView())
-    let textFieldsView = AnyView(TextFieldsView(shapes: .rounded))
-    let typographyView = AnyView(TypographyView())
+    let views: [(String, AnyView)] = [
+      ("ButtonsView", AnyView(ButtonsView(shapes: .rounded))),
+      ("CheckboxesView", AnyView(CheckboxesView())),
+      ("ColorsView", AnyView(ColorsView())),
+      ("RadiiView", AnyView(RadiiView())),
+      ("RadioGroupView", AnyView(RadioGroupView(shapes: .constant(.rounded)))),
+      ("ShadowsView", AnyView(ShadowsView())),
+      ("ShapesView", AnyView(ShapesView())),
+      ("SpacingsView", AnyView(SpacingsView())),
+      ("StrokesView", AnyView(StrokesView())),
+      ("TextFieldsView", AnyView(TextFieldsView(shapes: .rounded))),
+      ("TypographyView", AnyView(TypographyView()))
+    ]
 
-    let views: [AnyView] = [buttonsView, checkboxesView, colorsView, radiiView, radioGroupView, shadowsView, shapesView, spacingsView, strokesView, textFieldsView, typographyView]
-
-    for view in views {
+    for (name, view) in views {
       let hostingController = UIHostingController(rootView: view)
       hostingController.view.backgroundColor = .clear
-      assertSnapshots(of: hostingController, as: [String(describing: view): .image], record: false)
+      assertSnapshots(of: hostingController, as: [name: .image], record: true)
     }
   }
 }
